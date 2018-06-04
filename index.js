13a9029bbaebbf88cb5df3248e96f5124d9d8a9c
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-04 19:18:25 -0400

    Added additional functionality to the button

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ console.log('testing')
 
 const button = document.querySelector("button")
 
-// Changes the text of the button when it is clicked
+// Changes the text of the first h1 header and the header with the ID spells when it is clicked
 button.addEventListener("click", () => {
     let h1 = document.querySelector("h1")
     h1.textContent = "Expecto Patronum!"
+    
+    let spells = document.getElementById("spells")
+    spells.textContent = "BOOM!"
 })
