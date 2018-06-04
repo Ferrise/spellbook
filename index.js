21c809df5a135e7f62e00b5031f0df5e633fcf17
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-04 19:08:06 -0400

    Added functionality to the button that causes it to change the text of the Spellbook header

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('testing')
+
+const button = document.querySelector("button")
+
+// Changes the text of the button when it is clicked
+button.addEventListener("click", () => {
+    let h1 = document.querySelector("h1")
+    h1.textContent = "Expecto Patronum!"
+})
