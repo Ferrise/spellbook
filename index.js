fb6cc3fdae23d6792020278e1f97f008701c16be
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-04 22:47:16 -0400

    Added functionality to the submit button. It now changes the first header to whatever is in the text field whene it's clicked or enter is pressed within the text field.

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 console.log('testing')
 
 const button = document.querySelector("button")
+const h1 = document.querySelector("h1")
 
 // Changes the text of the first h1 header and the header with the ID spells when it is clicked
-button.addEventListener("click", () => {
-    let h1 = document.querySelector("h1")
+button.addEventListener("click", (e) => {
     h1.textContent = "Expecto Patronum!"
     
-    let spells = document.getElementById("spells")
+    const spells = document.getElementById("spells")
     spells.textContent = "BOOM!"
+})
+
+const submitButton = document.querySelector("#submit")
+
+// Changes the text of the first h1 header to whatever is in the text field and clears the text field
+submitButton.addEventListener("click", (e) => {
+    const textBox = document.getElementById("textBox")
+    h1.textContent = textBox.value
+    textBox.value = ""
+   
+})
+
+// Triggers a click event on the submit button when the user clicks enter in the text field
+document.addEventListener('keypress', (e) => {
+    const keyName = event.keyCode
+    if(keyName === 13){
+        document.querySelector("form").submit()
+    }
 })
