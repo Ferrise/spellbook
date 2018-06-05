c56f1353d7b6adc50d064701b48ecb9fda548026
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-05 15:04:16 -0400

    Clean up code

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const button = document.querySelector("button")
-const h1 = document.querySelector("h1")
+const button = document.querySelector('button')
+const h1 = document.querySelector('h1')
 
 // Changes the text of the first h1 header and the header with the ID spells when it is clicked
-button.addEventListener("click", (e) => {
-    h1.textContent = "Expecto Patronum!"
+button.addEventListener('click', (e) => {
+    h1.textContent = 'Expecto Patronum!'
     
-    const spells = document.getElementById("spells")
-    spells.textContent = "BOOM!"
+    const spells = document.getElementById('spells')
+    spells.textContent = 'BOOM!'
 })
 
-const form = doucment.querySelector('form')
+const form = document.querySelector('form')
 
 // Changes the text of the first h1 header to whatever is in the text field and clears the text field
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const textBox = document.getElementById("textBox")
-    h1.textContent = textBox.value
-    form.reset()
    
+    const f = e.target
+
+    h1.textContent = f.textBox.value
+    f.reset()
 })
