b414254e661a60266f7bad71a945588184697728
Ferris Esposito <ferrisesposito@gmail.com>
2018-06-09 17:38:03 -0400

    Create new list items by copying template

--- index.js
+++ index.js
@@ -3,28 +3,16 @@
 class SpellCaster{
     constructor(){
         this.spells = []
+        this.template = document.querySelector('.liTemplate')
+        this.list = document.querySelector('#spellList')
+
         const form = document.querySelector('#spellManipulators form')
         form.addEventListener('submit', (ev) => this.handleSubmit(ev))
     }
-    
-    renderProperty(className, textContent){
-        const span = document.createElement('span')
-        span.textContent = textContent
-        span.classList.add(className)
-        return span
-    }
 
-    renderDeleteButton(className, textContent){
-        const deleteButton = document.createElement('button')
-        deleteButton.textContent = textContent
-        deleteButton.classList.add(className)
-        deleteButton.addEventListener('click', (ev) => this.handleDeleteButton(ev))
-        return deleteButton
-    }
-
-    handleDeleteButton(ev){
+   deleteItem(ev){
         const deleteButton = ev.target
-        const parent = deleteButton.parentNode
+        const parent = deleteButton.closest('li')
         
         this.spells.splice(this.spells.indexOf(parent), 1)
         parent.parentNode.removeChild(parent)
@@ -32,14 +20,31 @@ class SpellCaster{
 
     renderItem(obj){
         const properties = Object.keys(obj)
-        const listItem = document.createElement('li')
+        
+        const listItem = this.template.cloneNode(true)
+        listItem.classList.remove('liTemplate')
     
         properties.forEach((property) => {
-            listItem.appendChild(this.renderProperty(property, obj[property] + " "))
+            let element = listItem.querySelector(`.${property}`)
+
+            //to handle different radio button checks
+            if(element){
+                element.textContent = obj[property] + " "
+            }else{
+                element = listItem.querySelector('.dark')
+                element.classList.remove('dark')
+                element.classList.add(property)
+                element.textContent = obj[property] + " "
+            }
         })
 
-        const deleteButton = this.renderDeleteButton('delete', 'Delete Spell')
-        listItem.appendChild(deleteButton)
+        //delete button
+        listItem
+            .querySelector('.delete')
+            .addEventListener('click',
+            (ev) => this.deleteItem(ev)
+        )
+        
         this.spells.push(listItem)
 
         return listItem
